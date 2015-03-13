26e19cc48fd4e52c90c8563f541e7ce24b829e8b
= <punketo28@gmail.com>
2015-03-13 12:57:34 -0430

    The init.el file was cleaned of unused libraries, and these libraries were deleted.

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ emacs-config
 
 My emacs configuration, simply locate this dir into your home dir. Rename it to .emacs.d
 
-INSTALL:
-For syntax highlighting for LESS CSS files clone https://github.com/purcell/less-css-mode.git into dir less-css-mode
-gettext-el (To enable major mode --PO mode-- and edit PO files).
+## INSTALL
+
+* For syntax highlighting for LESS CSS files clone https://github.com/purcell/less-css-mode.git into dir less-css-mode
 
 Emacs Rulez! 
--- init.el
+++ init.el
@@ -1,36 +1,10 @@
 (cd "~/.emacs.d/")
-;;;;;;;;;;;;;;;;;;;;;
-;; Enable php-mode ;;
-;;;;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/")
-(load "php-mode")
-(require 'php-mode)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Pear coding standards : http://pear.php.net/manual/en/standards.indenting.php ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun pear/php-mode-init ()
-  "Set some buffer-local variables."
-  (setq case-fold-search t)
-  (setq indent-tabs-mode nil)
-  (setq fill-column 78)
-  (setq c-basic-offset 4)
-  (c-set-offset 'arglist-cont 0)
-  (c-set-offset 'arglist-intro '+)
-  (c-set-offset 'case-label 2)
-  (c-set-offset 'arglist-close 0))
-(add-hook 'php-mode-hook 'pear/php-mode-init)
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enable color-theme ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'load-path "~/.emacs.d/color-theme")
 (require 'color-theme)
-(eval-after-load "color-theme"
-  '(progn
-     (color-theme-initialize)
-     (color-theme-dark-blue2)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enable emacs-color-theme-solarized ;;
@@ -41,11 +15,6 @@
   '(progn
      (color-theme-initialize)
      (color-theme-solarized)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; nxhtml for web development --nXhtml is an addon to Emacs for editing XHTML, PHP and similar things-- ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (load "~/.emacs.d/nxhtml/autostart.el")
 
 ;;;;;;;;;;;;;;;;;;;
 ;; less-css-mode ;;
