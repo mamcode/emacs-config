fa2fa88e764c112f189ce39471909bd0ab5f28c7
= <punketo28@gmail.com>
2015-03-14 16:31:14 -0430

    Tuning Emacs for improve code style in python

--- init.el
+++ init.el
@@ -42,3 +42,71 @@
 ;; Flycheck ;;
 ;;;;;;;;;;;;;;
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Enable syntax highlighting
+(global-font-lock-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Python specific settings ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;
+;; PEP 8 ;;
+;;;;;;;;;;;
+
+;; Don't use TABS for indentations.
+(setq-default indent-tabs-mode nil)
+
+;; Set the number to the number of columns to use.
+(setq-default fill-column 79)
+
+;; Add Autofill mode to mode hooks.
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+;; Show line number in the mode line.
+(line-number-mode 1)
+
+;; Show column number in the mode line.
+(column-number-mode 1)
+
+;; Indentation is automatically added.
+(load-library "python")
+
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
+(setq interpreter-mode-alist
+      (cons '("python" . python-mode)
+            interpreter-mode-alist)
+      python-mode-hook
+      '(lambda () (progn
+                    (set-variable 'py-indent-offset 4)
+                    (set-variable 'indent-tabs-mode nil))))
+
+;; Highlight the column where a line must stop
+;; Highlight character at "fill-column" position.
+(set-face-background 'column-marker-1 "red")
+(add-hook 'python-mode-hook
+          (lambda () (interactive)
+            (column-marker-1 fill-column)))
+
+;; Remove trailing whitespace manually by typing C-t C-w.
+(add-hook 'python-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-t C-w")
+                           'delete-trailing-whitespace)))
+
+;; Automatically remove trailing whitespace when file is saved.
+(add-hook 'python-mode-hook
+      (lambda()
+        (add-hook 'local-write-file-hooks
+              '(lambda()
+                 (save-excursion
+                   (delete-trailing-whitespace))))))
+
+;; Use M-SPC (use ALT key) to make sure that words are separated by
+;; just one space. Use C-x C-o to collapse a set of empty lines
+;; around the cursor to one empty line. Useful for deleting all but
+;; one blank line at end of file. To do this go to end of file (M->)
+;; and type C-x C-o.
